2b655a0f4f3b656d6a4ac57273a7f308455751d8
aminought <aminought@gmail.com>
2024-12-22 02:31:06 +0500

    fix navigator toolbox buttons

--- src/second_sidebar/xul/sidebar_main.mjs
+++ src/second_sidebar/xul/sidebar_main.mjs
@@ -5,6 +5,56 @@ export class SidebarMain extends Toolbar {
     super({ id: "sb2-main", classList: ["browser-toolbar"] });
     this.setMode("icons")
       .setContext("sb2-main-menupopup")
-      .setAttribute("customizable", "true");
+      .setAttribute("customizable", "true")
+      .addEventListener("mousedown", onMouseDown);
+  }
+}
+
+// https://searchfox.org/mozilla-central/source/browser/base/content/navigator-toolbox.js#106
+function onMouseDown(event) {
+  let element = event.target.closest(`
+        #firefox-view-button,
+        #alltabs-button,
+        #pageActionButton,
+        #downloads-button,
+        #fxa-toolbar-menu-button,
+        #unified-extensions-button,
+        #library-button
+        `);
+  if (!element) {
+    return;
+  }
+
+  switch (element.id) {
+    case "firefox-view-button":
+      FirefoxViewHandler.openToolbarMouseEvent(event);
+      break;
+
+    case "alltabs-button":
+      gTabsPanel.showAllTabsPanel(event, "alltabs-button");
+      break;
+
+    case "pageActionButton":
+      BrowserPageActions.mainButtonClicked(event);
+      break;
+
+    case "downloads-button":
+      DownloadsIndicatorView.onCommand(event);
+      break;
+
+    case "fxa-toolbar-menu-button":
+      gSync.toggleAccountPanel(element, event);
+      break;
+
+    case "unified-extensions-button":
+      gUnifiedExtensions.togglePanel(event);
+      break;
+
+    case "library-button":
+      PanelUI.showSubView("appMenu-libraryView", element, event);
+      break;
+
+    default:
+      throw new Error(`Missing case for #${element.id}`);
   }
 }
