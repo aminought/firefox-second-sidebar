703ffc3ab999a452a21f06a37fac4f9df5d4b78d
Benyamin Limanto <blbenyamin9@gmail.com>
2025-01-26 08:38:40 +0700

    feat(collapseable-sidebar): on hover, menu show, and sb2-box shown introduced

--- src/second_sidebar/css/sidebar_main.mjs
+++ src/second_sidebar/css/sidebar_main.mjs
@@ -4,9 +4,12 @@ export const SIDEBAR_MAIN_CSS = `
     flex-direction: column;
     justify-content: var(--sb2-main-web-panel-buttons-position);
     gap: var(--space-small);
-    padding: 0 var(--sb2-main-padding) var(--space-small) var(--sb2-main-padding);
+    padding: 0 0 0 4px;
     overflow-y: scroll;
     scrollbar-width: none;
+    width: 0px;
+    transition: width 0.5s ease-in-out, padding 0.5s ease-in-out;
+    transition-delay: 0.5s !important;
 
     toolbarpaletteitem[place="panel"][id^="wrapper-customizableui-special-spring"], toolbarspring {
       flex: 1;
@@ -23,13 +26,55 @@ export const SIDEBAR_MAIN_CSS = `
     .toolbarbutton-1 {
       padding: 0 !important;
     }
+
+    * {
+      margin-right: -15px;
+      transition: margin-right 0.5s ease-in-out;
+      transition-delay: 0.5s !important;
+    }
+  }
+
+
+  #sb2-main:hover, #sb2-main:active, #sb2-main:focus-within {
+    width: 48px;
+    padding: 0 var(--sb2-main-padding) var(--space-small) var(--sb2-main-padding);
+    transition: width 0.5s ease-in-out, padding 0.5s ease-in-out;
+    transition-delay: 0.5s !important;
+
+    * {
+      margin-right: 0px;
+      transition: margin-right 0.5s ease-in-out;
+    }
   }
 
   :root[customizing] {
     #sb2-main {
       min-width: unset !important;
+      max-width: unset !important;
+      width: unset !important;
+      padding: 0 var(--sb2-main-padding) var(--space-small) var(--sb2-main-padding);
+      * {
+        margin-right: 0px;
+      }
     }
   }
+
+  #browser:has(#sb2-box:not([hidden])), 
+  #browser:has(#sb2-main toolbarbutton[open]),
+  #main-window:has(#sb2-main-menupopup[hasbeenopened]:hover) {
+    #sb2-main {
+      min-width: unset !important;
+      max-width: unset !important;
+      width: unset !important;
+      padding: 0 var(--sb2-main-padding) var(--space-small) var(--sb2-main-padding);
+      transition: width 0.5s ease-in-out, padding 0.5s ease-in-out;
+      transition-delay: 0.5s !important;
+
+      * {
+        margin-right: 0px;
+      }
+    }
+}
 
   #browser:has(#sb2[position="right"]) #sb2-main {
     order: 7 !important;
