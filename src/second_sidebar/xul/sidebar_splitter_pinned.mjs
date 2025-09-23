c14aeb9004cd408c46927957a09db98c04d8e561
maya <99999855+roomfullofcommits@users.noreply.github.com>
2025-09-23 03:02:42 +0200

    fix sb2-splitter-pinned visible on browser start (#112)

--- src/second_sidebar/xul/sidebar_splitter_pinned.mjs
+++ src/second_sidebar/xul/sidebar_splitter_pinned.mjs
@@ -3,5 +3,6 @@ import { SidebarSplitter } from "./sidebar_splitter.mjs";
 export class SidebarSplitterPinned extends SidebarSplitter {
   constructor() {
     super({ id: "sb2-splitter-pinned" });
+    this.setAttribute("hidden", true);
   }
 }
