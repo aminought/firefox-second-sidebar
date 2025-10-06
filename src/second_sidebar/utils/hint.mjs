923a57cf6deef045af6edf66fc5f354c0d586c9e
aminought <aminought@gmail.com>
2025-10-06 06:44:55 +0300

    refactor hint

--- src/second_sidebar/utils/hint.mjs
+++ src/second_sidebar/utils/hint.mjs
@@ -2,33 +2,12 @@ import { SidebarControllers } from "../sidebar_controllers.mjs";
 import { SidebarElements } from "../sidebar_elements.mjs";
 
 export function showSidebarBoxPositionHint() {
-  /**
-   * @param {string} value
-   * @returns {string}
-   */
-  const parse = (value) => {
-    if (value === "unset") return null;
-    if (value.endsWith("px")) {
-      return Math.round(parseFloat(value)) + "px";
-    }
-    return "default";
-  };
-
-  const marginTop = parse(SidebarElements.sidebarBox.getProperty("margin-top"));
-  const marginLeft = parse(
-    SidebarElements.sidebarBox.getProperty("margin-left"),
-  );
-  const marginRight = parse(
-    SidebarElements.sidebarBox.getProperty("margin-right"),
-  );
-  const marginBottom = parse(
-    SidebarElements.sidebarBox.getProperty("margin-bottom"),
-  );
-  const width = parse(SidebarElements.sidebarBox.getProperty("width"));
-  const height = parse(SidebarElements.sidebarBox.getProperty("height"));
-
-  const webPanelController = SidebarControllers.webPanelsController.getActive();
-  const attach = webPanelController.getAttach();
+  const marginTop = parseProperty("margin-top");
+  const marginLeft = parseProperty("margin-left");
+  const marginRight = parseProperty("margin-right");
+  const marginBottom = parseProperty("margin-bottom");
+  const width = parseProperty("width");
+  const height = parseProperty("height");
 
   const parts = [];
   if (marginTop !== null) parts.push(marginTop);
@@ -36,6 +15,20 @@ export function showSidebarBoxPositionHint() {
   if (marginLeft !== null) parts.push(marginLeft);
   if (marginRight !== null) parts.push(marginRight);
 
-  const hint = `attach: ${attach}, offset: [${parts[0]} ${parts[1]}], size: [${width} ${height}]`;
+  const webPanelController = SidebarControllers.webPanelsController.getActive();
+  const hint = `attach: ${webPanelController.getAttach()}, offset: [${parts[0]} ${parts[1]}], size: [${width} ${height}]`;
   SidebarElements.sidebarHint.setText(hint);
+}
+
+/**
+ * @param {string} property
+ * @returns {string}
+ */
+function parseProperty(property) {
+  const value = SidebarElements.sidebarBox.getProperty(property);
+  if (value === "unset") return null;
+  if (value.endsWith("px")) {
+    return Math.round(parseFloat(value)) + "px";
+  }
+  return "default";
 }
