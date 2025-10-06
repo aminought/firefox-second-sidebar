a46852db55e5cdbde22f28507aedcb59e459ae01
aminought <aminought@gmail.com>
2025-10-06 05:33:33 +0500

    fix paths

--- src/second_sidebar/utils/files.mjs
+++ src/second_sidebar/utils/files.mjs
@@ -1,6 +1,6 @@
 import { ChromeRegistry } from "../wrappers/chrome_registry.mjs";
-import { FileProtocolHandler } from "../wrappers/file_protocol_handler.mjs";
 import { IOUtilsWrapper } from "../wrappers/io_utils.mjs";
+import { PathUtilsWrapper } from "../wrappers/path_utils.mjs";
 
 const CONTENT_URL = "chrome://userchrome/content/";
 
@@ -32,19 +32,11 @@ export async function removeFile(relativePath) {
  */
 function makePath(relativePath) {
   const contentDir = ChromeRegistry.convertChromeURL(CONTENT_URL);
-  const parentDir = contentDir.QueryInterface(Ci.nsIFileURL).file.parent;
-  const resourceFilePath = FileProtocolHandler.getURLSpecFromDir(parentDir);
-  const resourcePath = removeFilePrefix(resourceFilePath);
-  return resourcePath + relativePath;
-}
-
-/**
- *
- * @param {string} path
- * @returns {string}
- */
-function removeFilePrefix(path) {
-  return path.replace("file://", "");
+  const resourcePath = contentDir.QueryInterface(Ci.nsIFileURL).file.parent
+    .path;
+  const resourcePathParts = PathUtilsWrapper.split(resourcePath);
+  const relativePathParts = relativePath.split("/");
+  return PathUtilsWrapper.join(resourcePathParts.concat(relativePathParts));
 }
 
 /**
