daa802e01eeba2b92fb111bf3c14c71b9e84dbd1
Alexander Logumanov <aminought@gmail.com>
2026-09-13 16:16:18 +0500

    Fix sidebar search context action

--- src/second_sidebar/wrappers/search.mjs
+++ src/second_sidebar/wrappers/search.mjs
@@ -4,10 +4,10 @@ export class SearchService {
    * @param {string} query
    * @returns {Promise<string?>}
    */
-  static async getDefaulSubmissionUrl(query) {
-    const defaultEngine = Services.search.defaultEngine;
+  static async getDefaultSubmissionUrl(query) {
+    const defaultEngine = await Services.search.getDefault();
     if (!defaultEngine) return null;
-    const submission = await defaultEngine.getSubmission(query);
-    return submission.uri.spec;
+    const submission = defaultEngine.getSubmission(query);
+    return submission?.uri?.spec ?? null;
   }
 }
